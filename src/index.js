e68b3b57b220fa76bdb08dba3e85503b508a6557
Darin Williams <darinjswilliams@icloud.com>
2019-12-09 19:53:02 -0600

    Add files via upload
    updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-// import * as serviceWorker from './utils/serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
