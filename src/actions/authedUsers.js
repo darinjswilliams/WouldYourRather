18f954feeda4be40364a7514b498023bb4d3f64e
Darin Williams <darinjswilliams@icloud.com>
2019-12-09 19:45:43 -0600

    Add files via upload
    reloaded actions files

--- src/actions/authedUsers.js
+++ src/actions/authedUsers.js
@@ -1,5 +1,6 @@
 export const SET_AUTHED_USER = "SET_AUTHED_USER"
 
+
 export function setAuthUser(authUser) {
     return {
         type : SET_AUTHED_USER,
--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,6 +1,7 @@
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS"
 export const SAVE_QUESTION_ANSWER = "SAVE_QUESTION"
 
+
 export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
